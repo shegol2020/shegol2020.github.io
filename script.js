241ea167caf8d715d798dc13782e66775e436449
shegol2020 <70282983+shegol2020@users.noreply.github.com>
2020-10-22 10:59:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,103 +20,17 @@ function makeGETRequest(url, callback) {
         xhr.send();
      }
 
-//     function makeGETRequest2(url) {
-//     return new Promise((resolve, reject) => {
-//          }
-//     })
-// }
+    
+let array = [];
 
-class GoodsItem {
-    constructor(title, price) {
-        this.product_name = title;
-        this.price = price;
-    }
-    render() {
-        return `<div class="goods-item"><h3>${this.last_name}</h3><p>${this.first_name}</p></div>`;
-    }
-}
-class GoodsList {
-    constructor() {
-        this.goods = [];
-    }
-    fetchGoods(cb) {
+    fetchGoods() {
         makeGETRequest(API_URL, (goods) => {
-            this.goods = JSON.parse(goods);
-            console.log(this.goods);
-            cb();
-        })
-    }
-    render() {
-        let listHtml = '';
-        this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.first_name, good.last_name);
-            listHtml += goodItem.render();
+            array = JSON.parse(goods);
+            console.log(array);
+           
         });
-        document.querySelector('.goods-list').innerHTML = `<h2>Товары</h2> ${listHtml}`
-    }
-
-    calculateTotalCost () {
-        let totalCost=0;
-        this.goods.forEach((good) =>{
-            console.log(good.price);
-            totalCost += good.price;
-        });
-        console.log(totalCost);
-    }
-}
+    
 
 //---------------------------------------------------------------------------
-let myGoodList = new GoodsList();
-
-myGoodList.fetchGoods(() => {
-    myGoodList.render();
-});
-// console.log(myGoodList);
-
-
-
-myGoodList.calculateTotalCost()
-
+fetchGoods()
 //--------------------------------------------------------------------------
-class Basket {
-    constructor() {
-        this.basketGoods = [];
-    }
-    fetchBasket(cb) {
-        makeGETRequest(`${API_URL}/getBasket.json`).then((goods) => {
-            this.basketGoods = JSON.parse(goods);
-            console.log(this.basketGoods);
-            cb();
-        })
-    }
-
-    render() {
-        let listHtml = '';
-        this.basketGoods.contents.forEach(good => {
-            const basketItem = new BasketItem(good.product_name, good.price,good.quantity);
-            listHtml += basketItem.render();
-        });
-        document.querySelector('.goods-basket').innerHTML = `<h2>Корзина</h2> ${listHtml}`;
-    }
-
-}
-class BasketItem {
-    constructor(product_name, price, quantity) {
-        this.product_name = product_name;
-        this.price = price;
-        this.quantity = quantity;
-    }
-    summ() {
-        return this.price*this.quantity;
-    }
-    render() {
-        return `<div class="basket-list"><h3>${this.product_name}</h3><p>${this.price} <b>${this.quantity} </b></p></div>`;
-    }
-}
-
-let myBasket = new Basket();
-
-myBasket.fetchBasket(() => {
-    myBasket.render();
-});
-console.log(myBasket);
