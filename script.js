98599bbefadaef568d6ced47edd8dd335fcaaaeb
shegol2020 <70282983+shegol2020@users.noreply.github.com>
2020-10-22 10:51:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = '&access_token=16ccccb8e628800ba653f224294b50b8d46f4f64a6b45efadd82f2e7b86fb48cd5eea354a28b9445ec72f';
+const API_URL = 'https://api.vk.com/method/users.get?user_id=210700286&v=5.52&access_token=16ccccb8e628800ba653f224294b50b8d46f4f64a6b45efadd82f2e7b86fb48cd5eea354a28b9445ec72f';
 
 function makeGETRequest(url, callback) {
         var xhr;
