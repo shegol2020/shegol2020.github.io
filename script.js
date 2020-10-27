c21a0087e894de4cdea80d9856a68d18899a9cd6
shegol2020 <70282983+shegol2020@users.noreply.github.com>
2020-10-27 15:41:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
-const API_URL = 'https://api.vk.com/method/users.get?user_id=210700286&v=5.52&access_token=16ccccb8e628800ba653f224294b50b8d46f4f64a6b45efadd82f2e7b86fb48cd5eea354a28b9445ec72f';
+    var user = [];
+    var script = document.createElement('SCRIPT');
+    script.src = "https://api.vk.com/method/users.get?user_ids=254&fields=photo_100&v=5.124&callback=callbackFunc&access_token=ad29adf1ad29adf1ad29adf1efad371086aad29ad29adf1f67c3f137270830a839f4c27";
+    console.log(script.src);
+    document.getElementsByTagName("head")[0].appendChild(script);
+    function callbackFunc(result) {
+        user=result;
+        console.log(user);
+        alert(result.response[0].first_name+' '+result.response[0].last_name)  
+    }
 
-function makeGETRequest(url, callback) {
-        var xhr;
 
-        if (window.XMLHttpRequest) {
-            xhr = new XMLHttpRequest();
-        } else if (window.ActiveXObject) {
-            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    class User {
+        constructor(first_name, last_name) {
+            this.first_name = first_name;
+            this.last_name = last_name;
         }
+        renderUser() {
+            return `<div class="user-info"><h3>${this.first_name}</h3><p>${this.last_name}</p></div>`;                   
+        }
+    }
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-            // console.log('-----------');
-            callback(xhr.responseText);
+    class UserList {
+        constructor() {
+            this.goods = [];
+        }
+        fetchUser(cb) { 
+            this.list = user.response;
+            cb();
+        }
+        renderList() {
+            let listHtml = '';
+            //console.log(this.list);
+            //console.log(this.list.first_name);
+            this.list.forEach(i => {
+                    const userInfo = new User(i.first_name, i.last_name);    
+                    listHtml += userInfo.renderUser();
+                });
+            document.querySelector('.user-list').innerHTML = `<h2>Пользователи</h2> ${listHtml}`
             }
-        }
+    }
+function f1() {
+    let myUser = new UserList();
 
-        xhr.open('GET', url, true);
-        xhr.send();
-     }
-
-    
-let array = [];
-
-    fetchGoods() {
-        makeGETRequest(API_URL, (goods) => {
-            array = JSON.parse(goods);
-            console.log(array);
-           
-        });
-    
-
-//---------------------------------------------------------------------------
-fetchGoods()
-//--------------------------------------------------------------------------
+    myUser.fetchUser(() => {
+        myUser.render();
+    })
+}
