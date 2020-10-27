861516ecd0893c0d50bd862086d465dc98733224
shegol2020 <70282983+shegol2020@users.noreply.github.com>
2020-10-27 16:33:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     var user = [];
     var script = document.createElement('SCRIPT');
-    script.src = "https://api.vk.com/method/users.get?user_ids=254,193990&fields=city&fields=photo_100&v=5.124&callback=callbackFunc&access_token=ad29adf1ad29adf1ad29adf1efad371086aad29ad29adf1f67c3f137270830a839f4c27";
+    script.src = "https://api.vk.com/method/users.get?user_ids=254,193990&fields=photo_100&v=5.124&callback=callbackFunc&access_token=ad29adf1ad29adf1ad29adf1efad371086aad29ad29adf1f67c3f137270830a839f4c27";
     console.log(script.src);
     document.getElementsByTagName("head")[0].appendChild(script);
     function callbackFunc(result) {
@@ -9,15 +9,14 @@
 
 
     class User {
-        constructor(first_name, last_name, city, photo_100) {
+        constructor(first_name, last_name, photo_100) {
             this.first_name = first_name;
             this.last_name = last_name;
-            this.city = city;
             this.photo_100 = photo_100;
             
         }
         renderUser() {
-            return `<div class="user-info"><p>${this.first_name} ${this.last_name} ${this.city.title}</p>
+            return `<div class="user-info"><p>${this.first_name} ${this.last_name}</p>
  <div class="users-photo"><img src="${this.photo_100.url}"</div>
 </div>`;                   
         }
@@ -36,7 +35,7 @@
             //console.log(this.list);
             //console.log(this.list.first_name);
             this.list.forEach(i => {
-                    const userInfo = new User(i.first_name, i.last_name, i.city, i.photo_100);    
+                    const userInfo = new User(i.first_name, i.last_name, i.photo_100);    
                     listHtml += userInfo.renderUser();
                 });
             document.querySelector('.users-list').innerHTML = `${listHtml}`
